37118bfc6a8ca71b048a398434a61d9106543956
Mudit <itzmuditgarg@gmail.com>
2020-02-29 10:44:01 +0530

    Tic-tac-toe game with pwa support hosted on firebase at (https://tic-tac-toe-de979.firebaseapp.com/)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { SquareComponent } from "./square/square.component";
+import { BoardComponent } from "./board/board.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import { AppRoutingModule } from "./app-routing.module";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SquareComponent, BoardComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: "cosmic" }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    AppRoutingModule,
+    NbButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
